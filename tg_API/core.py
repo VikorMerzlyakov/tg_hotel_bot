6277dcc8647efe5eeb2f6976ced99acda494b234
VikorMerzlyakov <111169794+VikorMerzlyakov@users.noreply.github.com>
2025-03-12 19:08:15 +0300

    API(rapid)

--- tg_API/core.py
+++ tg_API/core.py
@@ -1,16 +1,59 @@
+import os
+from dotenv import load_dotenv
 import requests
 
-from settings import SiteSetting
+load_dotenv()
 
 url = "https://booking-com15.p.rapidapi.com/api/v1/attraction/getAttractionReviews"
 
-querystring = {"id":"PR6K7ZswbGBs","page":"1"}
+querystring = {"id": "PR6K7ZswbGBs", "page": "1"}
+
 
 headers = {
-	"x-rapidapi-key": "2cdea864d3msh2f2d1b647f2ba0ap15b054jsne3e9370ebbad",
-	"x-rapidapi-host": "booking-com15.p.rapidapi.com"
+    "x-rapidapi-key": os.getenv("SITE_API"),
+    "x-rapidapi-host": "booking-com15.p.rapidapi.com"
 }
+
 
 response = requests.get(url, headers=headers, params=querystring)
 
-print(response.text)
+
+if response.status_code == 200:
+    try:
+        # Парсим JSON-ответ
+        data = response.json()
+
+        # Извлекаем нужные данные
+        reviews = []
+        data_list = data.get("data")  # Получаем список данных
+
+        for item in data_list:
+
+            review_id = item.get("id")
+            numeric_rating = item.get("numericRating")
+            content = item.get("content")
+            user_info = item.get("user")
+
+            user_name = user_info.get("name") if isinstance(user_info, dict) else None
+
+            review = {
+                "id": review_id,
+                "numericRating": numeric_rating,
+                "content": content,
+                "userName": user_name
+            }
+            reviews.append(review)
+
+
+            print("Отзывы:")
+            for review in reviews:
+                print(f"ID: {review['id']}")
+                print(f"Оценка: {review['numericRating']}")
+                print(f"Текст: {review['content']}")
+                print(f"Пользователь: {review['userName']}")
+                print("-" * 40)
+
+    except Exception as e:
+        print(f"Ошибка при обработке данных: {e}")
+else:
+    print(f"Ошибка при запросе к API: {response.status_code}")
